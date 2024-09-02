2ec034b1782c7a7462bb6d9d4b4c344d66d0c5b5
Serhii Sudarenko <159451661+SergeySudaraenko@users.noreply.github.com>
2024-09-02 18:47:47 +0200

    Delete src directory

--- src/config/messages.py
+++
@@ -1,6 +0,0 @@
-ACCOUNT_EXIST="Account already exists!"
-INVALID_EMAIL="Invalid email!"
-NOT_CONFIRMED_EMAIL="Email is not confirmed!"
-INVALID_PASSWORD="Invalid password!"
-INVALID_REF_TOKEN="Invalid refresh token!"
-VERIFICATION_ERROR="Verification error!"
